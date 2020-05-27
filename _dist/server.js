788eb8b99022f0f939b823bd85ae54f100c8a9d1
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-27 20:32:18 -0300

    Add - Procfile

--- _dist/server.js
+++ _dist/server.js
@@ -4,4 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-app_1.default.listen(3333);
+app_1.default.listen(process.env.PORT || 3333);
